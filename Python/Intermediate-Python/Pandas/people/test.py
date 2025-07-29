746002ea5296668feb4fcd4c93a14494163f002b
saiimonn <gementizasgg08@gmail.com>
2025-07-29 19:46:09 +0800

    refactor: dictionaries and pandas in one folder

--- Python/Intermediate-Python/Pandas/people/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-print(os.getcwd())
-
-filePath = "Intermediate Python/CSV-Files"
-
-var = pd.read_csv("Python/Intermediate-Python/CSV-Files/people.csv", index_col = 0)
-print(var)
